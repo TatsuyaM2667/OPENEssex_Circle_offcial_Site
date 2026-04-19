532f44c8be98032c7a3cac84c488cfcccefb69cd
TatsuyaM2667 <miuranosuketatsuya06@gmail.com>
2026-04-20 00:20:16 +0900

    エラー修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Documents from './pages/Documents';
